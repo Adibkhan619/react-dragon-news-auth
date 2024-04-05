7931606b2dd6a29e850cb2f5e13446621648cbe7
Adibkhan619 <work.adibkhan@gmail.com>
2024-04-06 00:18:19 +0600

    added right nav section

--- src/pages/RightNavBar/RIghtNavBar.jsx
+++ src/pages/RightNavBar/RIghtNavBar.jsx
@@ -1,11 +1,21 @@
-
+import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa6";
 
 const RIghtNavBar = () => {
-    return (
-        <div>
-            <h2 className="text-2xl"> I am right Navbar</h2>
-        </div>
-    );
+  return (
+    <div>
+      <div className="border p-4 space-y-3">
+        <h2 className="text-2xl">Find Us On</h2>
+        <button className="btn w-full">
+          <FaGoogle />
+          Login with Google
+        </button>
+        <button className="btn w-full">
+          <FaGithub />
+          Login with facebook
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default RIghtNavBar;
